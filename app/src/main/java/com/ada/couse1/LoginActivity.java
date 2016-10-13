fef712bbe31e1aa1fea1950138a9b7da66868949
Adriana Jazbor-Indyk <jazboria@gene.com>
2016-10-12 22:51:31 -0700

    first commit

---
+++ app/src/main/java/com/ada/couse1/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.ada.couse1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+/*
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.rlLogin);
+        VideoView video = (VideoView) findViewById(R.id.videoView);
+        Uri uri = Uri.parse("http://techslides.com/demos/sample-videos/small.mp4");
+        video.setVideoURI(uri);
+        //video.setLayoutParams(new FrameLayout.LayoutParams(550, 550));
+        layout.addView(video);*/
+
+    }
+}
